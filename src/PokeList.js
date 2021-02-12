31a2502e2acab9f37c843331c5bf1e629f6320a9
Casey Warren <caseywar1017@gmail.com>
2021-02-12 18:21:06 -0500

    sort and ascend functions working

--- src/PokeList.js
+++ src/PokeList.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import PokeItem from './PokeItem.js'
+import Dropdown from './Dropdown.js'
+import pokemon from './pokemon.js'
 
 
 export default class PokeList extends Component {
     state = {
         pokemon: [],
         query: '',
+        sortBy: 'pokemon',
+        sortOrder: 'Ascend'
     }
 
     handleInputChange = (e) => {
@@ -13,11 +17,48 @@ export default class PokeList extends Component {
             query: e.target.value,
         });
     }
+
+
+
+    handleDirectionSort = (e) => {
+        this.setState({
+            sortOrder: e.target.value,
+        });
+    }
+
+    handleSortBy = (e) => {
+        this.setState({
+            sortBy: e.target.value,
+        });
+    }
+
+
     render() {
+        if (this.state.sortOrder === 'Ascend') {
+            pokemon.sort(
+                (a, b) =>
+                    a[this.state.sortBy].localeCompare(b[this.state.sortBy])
+            );
+        }
+        if (this.state.sortOrder === 'Descend') {
+            pokemon.sort(
+                (a, b) =>
+                    b[this.state.sortBy].localeCompare(a[this.state.sortBy])
+            );
+        }
+
+
         const filteredPokemon = this.props.pokemon.filter(pokemon => pokemon.pokemon.includes(this.state.query))
 
         return (
-            <>
+            <form>
+
+                <div>
+                    <Dropdown currentValue={this.state.sortOrder} handleChanges={this.handleDirectionSort} options={['Ascend', 'Descend']} />
+                    <Dropdown currentValue={this.state.sortOrder} handleChanges={this.handleSortBy} options={['pokemon', 'type_1']} />
+                </div>
+
+
                 <input onChange={this.handleInputChange} />
 
                 <ul className='list' >
@@ -29,7 +70,7 @@ export default class PokeList extends Component {
                             />)
                     }
                 </ul>
-            </>
+            </form>
         )
     }
 }
