ff76e26bca3c7c2d2f0a3f75e0bf1858699b62af
Casey Warren <caseywar1017@gmail.com>
2021-02-11 19:12:20 -0500

    added page links to top of page using react router

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
+import { NavLink, withRouter } from 'react-router-dom'
+
 
 export default class Header extends Component {
     render() {
         return (
-            <div>
-                hello moto
-            
-
-            </div>
+            <header>
+                <NavLink exact activeClassName="selected" to="/">
+                    Home
+            </NavLink>
+                <NavLink exact activeClassName="selected" to="/search">
+                    Search Page
+            </NavLink>
+                {/* {
+                    this.props.location.pathname !== '/search' && <NavLink exact activeClassName="selected" to="/search">
+                        Search
+                </NavLink>
+                } */}
+                {/* {
+                    this.props.location.pathname === '/search' && <a href="https://www.pokemon.com/us/pokedex/">Official Pokedex™ Website </a>
+                } */}
+            </header>
         )
     }
 }
