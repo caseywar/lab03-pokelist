3d6dc36185ad70e825f80f8cf05fc8922afed43e
Casey Warren <caseywar1017@gmail.com>
2021-02-12 20:37:39 -0500

    fixed some styles

--- src/PokeItem.js
+++ src/PokeItem.js
@@ -7,7 +7,7 @@ export default class PokeItem extends Component {
             <li className="pokemonItems">
                 <p className="title">{this.props.pokemomItemProp.pokemon}</p>
                 <img src={this.props.pokemomItemProp.url_image} alt={this.props.pokemomItemProp.pokemon}></img>
-                <p className="typeText">POKEMON TYPE: {this.props.pokemomItemProp.type_1}</p>
+                <p className="typeText">TYPE: {this.props.pokemomItemProp.type_1}</p>
                 <p className="typeText">SHAPE: {this.props.pokemomItemProp.shape}</p>
                 <p className="typeText">ABILITY: {this.props.pokemomItemProp.ability_1}</p>
 
