98a512b79e47a052a6fd6ca798d70bc77ce2b92f
Casey Warren <caseywar1017@gmail.com>
2021-02-16 18:46:08 -0500

    got pokemon rendering and search bar working

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react'
-import PokeList from './PokeList.js'
+// import PokeList from './PokeList.js'
 import pokemon from './pokemon.js'
 import './App.css';
-import Dropdown from './Dropdown.js'
+// import Dropdown from './Dropdown.js'
+import request from 'superagent';
+
 
 
 export default class SearchPage extends Component {
     state = {
         pokemon: [],
         query: '',
-        sortBy: 'pokemon',
-        sortOrder: 'Ascend'
+        // sortBy: 'pokemon',
+        // sortOrder: 'Ascend'
     }
 
-    handleInputChange = (e) => {
+    componentDidMount = async () => {
+        await this.fetchPokemon();
+    }
+
+    fetchPokemon = async () => {
+        const data = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${this.state.query}`);
+
+        this.setState({
+            pokemon: data.body.results,
+        });
+    }
+
+    handleClick = async () => {
+        await this.fetchPokemon();
+    }
+
+
+
+    handleInputChange = async (e) => {
         this.setState({
             query: e.target.value,
         });
@@ -21,52 +41,74 @@ export default class SearchPage extends Component {
 
 
 
-    handleDirectionSort = (e) => {
-        this.setState({
-            sortOrder: e.target.value,
-        });
-    }
+    // handleDirectionSort = (e) => {
+    //     this.setState({
+    //         sortOrder: e.target.value,
+    //     });
+    // }
 
-    handleSortBy = (e) => {
-        this.setState({
-            sortBy: e.target.value,
-        });
-    }
+    // handleSortBy = (e) => {
+    //     this.setState({
+    //         sortBy: e.target.value,
+    //     });
+    // }
 
     render() {
-        if (this.state.sortOrder === 'Ascend') {
-            pokemon.sort(
-                (a, b) =>
-                    a[this.state.sortBy].localeCompare(b[this.state.sortBy])
-            );
-        }
-        if (this.state.sortOrder === 'Descend') {
-            pokemon.sort(
-                (a, b) =>
-                    b[this.state.sortBy].localeCompare(a[this.state.sortBy])
-            );
-        }
+        // if (this.state.sortOrder === 'Ascend') {
+        //     pokemon.sort(
+        //         (a, b) =>
+        //             a[this.state.sortBy].localeCompare(b[this.state.sortBy])
+        //     );
+        // }
+        // if (this.state.sortOrder === 'Descend') {
+        //     pokemon.sort(
+        //         (a, b) =>
+        //             b[this.state.sortBy].localeCompare(a[this.state.sortBy])
+        //     );
+        // }
 
 
-        const filteredPokemon = pokemon.filter(pokemon => pokemon.pokemon.includes(this.state.query))
+        // const filteredPokemon = pokemon.filter(pokemon => pokemon.pokemon.includes(this.state.query))
 
 
         return (
             <>
 
                 <main className="searchMain">
-                    <form className="sidebar">
+                    <div className="sidebar">
                         <div>
+                            <label>
+                                Search
+                                    <input onChange={this.handleInputChange} />
+                            </label>
+
+
+
+
                             <span className="searchTitle">Search Pokemon</span>
 
-                            <Dropdown currentValue={this.state.sortOrder} handleChanges={this.handleDirectionSort} options={['Ascend', 'Descend']} />
-                            <Dropdown currentValue={this.state.sortOrder} handleChanges={this.handleSortBy} options={['Name', 'type_1', 'shape', 'ability_1']} />
+                            {/* <Dropdown currentValue={this.state.sortOrder} handleChanges={this.handleDirectionSort} options={['Ascend', 'Descend']} />
+                            <Dropdown currentValue={this.state.sortOrder} handleChanges={this.handleSortBy} options={['Name', 'type_1', 'shape', 'ability_1']} /> */}
                         </div>
+                    </div>
 
 
-                        <input onChange={this.handleInputChange} />
-                    </form>
-                    <PokeList filteredPokemon={filteredPokemon} />
+                    <div>
+
+                        <button onClick={this.handleClick}>Search</button>
+                        {
+                            this.state.pokemon.map(poke =>
+
+                                <div key={pokemon.pokemon}>
+                                    <div>
+                                        <img src={poke.url_image} alt="poke" />
+                                    </div>
+                                    {poke.pokemon} :{poke.type_1}
+                                </div>)
+                        }
+                    </div>
+
+                    {/* <PokeList filteredPokemon={filteredPokemon} /> */}
                 </main>
 
             </>
